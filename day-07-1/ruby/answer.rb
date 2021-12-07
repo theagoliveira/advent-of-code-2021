e8045faa106b0fc5967b02103b5039cf3304a54b
Thiago Cavalcante <thiago.kun@gmail.com>
2021-12-07 03:09:00 -0300

    Day 7 (ruby)

---
+++ day-07-1/ruby/answer.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class Answer
+  def self.read_input_file
+    File.read('input.txt')
+  end
+
+  def main
+    crab_positions = self.class.read_input_file.split(',').map(&:to_i)
+    positions_count_hash = Hash.new(0)
+    crab_positions.each { |position| positions_count_hash[position] += 1 }
+
+    min_fuel = -1
+    beginning = positions_count_hash.keys.min
+    ending = positions_count_hash.keys.max
+
+    beginning.upto(ending) do |position|
+      current_fuel = calculate_fuel(positions_count_hash, position)
+      min_fuel = current_fuel if current_fuel < min_fuel || min_fuel.negative?
+    end
+
+    min_fuel
+  end
+
+  def calculate_fuel(positions_count_hash, target_position)
+    positions_count_hash.reduce(0) do |fuel, (position, count)|
+      fuel + (position - target_position).abs * count
+    end
+  end
+end
+
+puts Answer.new.main
---
+++ day-07-1/ruby/answer_test.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative 'answer'
+
+class AnswerTest < Minitest::Test
+  EXAMPLE = '16,1,2,0,4,2,7,1,2,14'
+
+  def test_main
+    # skip
+    Answer.stub(:read_input_file, EXAMPLE) do
+      assert_equal 37, Answer.new.main
+    end
+  end
+end
